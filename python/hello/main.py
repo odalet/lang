0bff29296563d013d24e8d1c88013b236c6daac5
Olivier DALET <olivier.dalet@addupsolutions.com>
2024-06-24 15:04:58 +0200

    Python tests

---
+++ python/hello/main.py
@@ -0,0 +1,9 @@
+﻿# This is a sample Python script.
+# https://www.jetbrains.com/help/pycharm/
+
+def say_hello(name: str) -> None:
+    print(f"Hello, {name}")
+
+
+if __name__ == "__main__":
+    say_hello("PyCharm")

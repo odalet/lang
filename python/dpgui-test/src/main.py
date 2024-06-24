9c678e8e2089b47f12f721af7676db076974e045
Olivier DALET <olivier.dalet@addupsolutions.com>
2024-06-24 16:35:45 +0200

    dearpygui

---
+++ python/dpgui-test/src/main.py
@@ -0,0 +1,21 @@
+﻿import dearpygui.dearpygui as dpg
+
+
+def main() -> None:
+    dpg.create_context()
+    dpg.create_viewport(title='Custom Title', width=600, height=300)
+
+    with dpg.window(label="Example Window"):
+        dpg.add_text("Hello, world")
+        dpg.add_button(label="Save")
+        dpg.add_input_text(label="string", default_value="Quick brown fox")
+        dpg.add_slider_float(label="float", default_value=0.273, max_value=1)
+
+    dpg.setup_dearpygui()
+    dpg.show_viewport()
+    dpg.start_dearpygui()
+    dpg.destroy_context()
+
+
+if __name__ == '__main__':
+    main()
